d97bc9af6103f303fc1633ff39477a8caa57f1dd
huangjie <841440416@qq.com>
2022-04-09 23:48:36 +0800

    feat: 依赖收集

--- packages/reactivity/src/baseHandler.ts
+++ packages/reactivity/src/baseHandler.ts
@@ -1,3 +1,5 @@
+import { track, trigger } from "./effect";
+
 export const enum ReactiveFlags {
   IS_REACTIVE = '__v_isReactive'
 }
@@ -6,10 +8,18 @@ export const mutableHandlers = {
     if(key ==  ReactiveFlags.IS_REACTIVE) {
       return true;
     }
+    // 依赖收集
+    track(target, 'get', key);
     // reflect会将原对象target改成代理对象
     return Reflect.get(target, key, receiver);
   },
   set(target, key, value, receiver) {
-    return Reflect.set(target, key, value, receiver);
+    const oldValue = target[key];
+    const result = Reflect.set(target, key, value, receiver);
+    if(oldValue !== value) {
+      // 更新
+      trigger(target, 'set', key, value, oldValue);
+    }
+    return result;
   }
 }
--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,20 +1,23 @@
 export let activeEffect = undefined;
 class ReactiveEffect {
+  public deps = []; // 记录依赖的属性
+  public parent = null; // 父节点
   public active = true; // 实例上新增了active属性
   constructor(public fn) { // public：用户传递的参数会挂载this this.fn = fn
   }
   run() {
     // 未激活执行执行函数，无需依赖收集
     if (!this.active) {
-      this.fn();
-      return;
+      return this.fn();
     }
     try {
+      this.parent = activeEffect;
       // 激活执行执行函数，依赖收集，将当前的effect和稍后渲染的属性关联
       activeEffect = this;
       return this.fn();
     } finally {
-      activeEffect = undefined;
+      activeEffect = this.parent; // 执行完还原
+      this.parent = null;
     }
   }
 }
@@ -24,3 +27,45 @@ export function effect(fn) {
   const _effect = new ReactiveEffect(fn);
   _effect.run(); // 默认执行异常
 }
+
+// effect和属性是多对多关系
+const targetMap = new WeakMap();
+export function track(target, type, key) {
+  if(!activeEffect) return;
+  let depsMap = targetMap.get(target);
+  if(!depsMap) {
+    targetMap.set(target, depsMap = new Map());
+  }
+  let dep = depsMap.get(key);
+  if(!dep) {
+    depsMap.set(key, dep = new Set());
+  }
+  let shouldTrack = !dep.has(activeEffect);
+  if(shouldTrack) {
+    dep.add(activeEffect);
+    activeEffect.deps.push(dep); // effect反向记录dep，以便清除
+  }
+}
+
+export function trigger(target, type, key, value, oldValue) {
+  const depsMap = targetMap.get(target);
+  if(!depsMap) return; // 触发的值不在模板中使用
+  const effects = depsMap.get(key); // 找到对应的effect
+  effects && effects.forEach(effect => {
+    // 执行effect的时候，需判断是否执行过，如果执行过，则不再执行
+    if(effect !== activeEffect) effect.run();
+  })
+}
+
+
+// 1. 创建一个响应式对象 new proxy
+// 2. 创建一个effect, 默认数据变化渲染更新，将正在执行的effect作为全局变量，
+// 渲染（取值），在get方法中调用tracker方法中进行依赖收集
+// 3. weakMap，将effect和属性关联
+// 4. 用户修改数据，调用triggr方法，触发effect，effect执行
+
+
+
+
+// 嵌套执行实现：
+// 1. 给ReactiveEffect类标记父亲，构造树形结构
